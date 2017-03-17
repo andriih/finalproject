c519d73377175550d4f68b81c62b88aaacb89e05
andriih <andrii.hnatyshyn@gmail.com>
2017-03-17 16:46:17 +0200

    parallax: problem with centering

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 $(window).scroll(function(){
 	var st  = $(this).scrollTop();
-
+	$('.parallax').css({
+		"transform":"translate(0%, "+st/6+"%"
+	});
 	$('.parallax__area__title').css({
 		"transform":"translate(0%, "+st / 6+"%"
 	});
