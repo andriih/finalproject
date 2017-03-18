eea6ce431520b5d523cfd48a5a08b94fe8c7a903
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-03-18 09:15:18 +0200

    backup

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,11 @@
 $(window).scroll(function(){
 	var st  = $(this).scrollTop();
-	$('.parallax').css({
-		"transform":"translate(0%, "+st/6+"%"
-	});
+
 	$('.parallax__area__title').css({
-		"transform":"translate(0%, "+st / 6+"%"
+		"transform":"translate(0%, "+st / 8+"%"
 	});
 	$('.parallax__area__carousel').css({
-		"transform":"translate(0%, "+st / 5 +"%"
+		"transform":"translate(0%, "+st / 6 +"%"
 	});
 });
 
