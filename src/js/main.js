cb567f9f0aad42af1ffd4880b0e84321625e933a
andriih <andrii.hnatyshyn@gmail.com>
2017-03-19 18:22:35 +0200

    validation added

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,112 @@ $('.grid').masonry({
   percentPosition: true
 });
 
+
+var headerForm = $("#header-form");
+
+
+///HEADER FORM
+
+$("#header-form").validate({
+    rules: {
+        name: {
+            lettersonly: true,
+            minlength: 3,
+            maxlength: 15,
+            required: true
+        },
+        email: {
+            required: true
+        },
+        number: {
+            minlength: 10,
+            maxlength: 15,
+            required: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'div',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    },
+    submitHandler: function(form) {
+
+       // $("#header-form").reset();
+
+        toastr.success('Отправлено!');
+        $("#header-form")[0].reset();
+        $('#myModal').removeClass('in').attr('style','display:none;');
+        $(".modal-backdrop.fade.in").remove();
+        //$('.modal-backdrop').removeClass('modal-backdrop fade in');
+        //$('#myModal').attr('style','display:none');
+
+        $('body').removeAttr('class').removeAttr('style');
+
+
+
+    }
+});
+
+///FOOTER FORM
+
+$("#footer-form").validate({
+    rules: {
+        name: {
+            lettersonly: true,
+            minlength: 3,
+            maxlength: 15,
+            required: true
+        },
+        email: {
+            required: true
+        },
+        number: {
+            minlength: 10,
+            maxlength: 15,
+            required: true
+        },
+        question:{
+            required: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'div',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    },
+    submitHandler: function(form) {
+
+        // $("#header-form").reset();
+
+        toastr.success('Вопрос отправлен!');
+        $("#footer-form")[0].reset();
+        $('#myModal__question').removeClass('in').attr('style','display:none;');
+        $(".modal-backdrop.fade.in").remove();
+
+
+        $('body').removeAttr('class').removeAttr('style');
+
+
+
+    }
+});
