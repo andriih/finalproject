ccb6cae7da7406f3e110c93d217c40f8cdae8a71
andriih <andrii.hnatyshyn@gmail.com>
2017-03-22 12:03:34 +0200

    smooth scrolling added

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
     $("#demo04").animatedModal();
 });
 
+$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
 
 
 $(window).scroll(function(){
